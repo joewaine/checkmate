dc8a59ba9b5ea4b20bfd87fefde92158cc599658
Joseph Waine <joe.waine@gmail.com>
2013-03-19 16:11:36 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkmate::Application.config.secret_token = '3f23547f9d2a8a9fb6b6f7f32434a79270c0f5fee3aaae7f716b5a904c81b069fb58c8d4abee241ac342af5509860f0e65c9b5c6dbd5a3c04fbc6bf43dd2a812'
