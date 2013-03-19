35ecdce2eb64b92273e544e0516eefeab26fbd02
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2013-03-19 18:45:46 -0400

    fixed seed file from simon

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,4 @@
 Checkmate::Application.routes.draw do
-<<<<<<< HEAD
   root :to => 'home#index'
   resources :accounts, :users, :transactions
-=======
-<<<<<<< HEAD
-
-  root :to => 'home#index'
-  resources :transactions
-=======
-  root :to => 'home#index'
-  resources :accounts
-  resources :users
-  resources :transaction
->>>>>>> development
->>>>>>> cfb66f8dd6f48f4dce064c390736d791b8c2bc8b
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ a1 = Account.create(:name => 'Chase', :balance => 98.00, )
 
 t1 = Transaction.create(:date => '1982/07/15', amount: 71.00, description: 'check', is_deposit: true)
 t2 = Transaction.create(:date => '1999/11/20', amount: 61.00, description: 'cash', is_deposit: false)
-t3 = Transaction.create(:date => '2001/03/11', amount: => 38.00, description: => 'check', is_deposit: => true)
+t3 = Transaction.create(:date => '2001/03/11', amount: 38.00, description: => 'check', is_deposit: true)
 t4 = Transaction.create(:date => '2014/4/27', amount: 98.00, description: 'cash', is_deposit: true)
 
 u1 = User.create(:email => 'abc@gmail.com', :password => 'a', :password_confirmation => 'a' )
